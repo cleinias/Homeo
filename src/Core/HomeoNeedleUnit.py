efcaa9763c59744f7a62e4d17a1fb71a23158b5d
Stefano <stefano.franchi@gmail.com>
2013-02-21 22:39:23 -0600

    more work on HomeoUnit

--- src/Core/HomeoNeedleUnit.py
+++ src/Core/HomeoNeedleUnit.py
@@ -1,5 +1,17 @@
 '''
-Created on Feb 19, 2013
+Created on Feb 21, 2013
 
 @author: stefano
 '''
+
+class HomeoNeedleUnit:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
