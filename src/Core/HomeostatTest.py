df6c76a40e60d0e66961f9e3de65d1f0dc08a096
Stefano <stefano.franchi@gmail.com>
2013-03-11 15:25:27 -0500

    Ported all tests in HomeConnectionTest Smalltalk code

---
+++ src/Core/HomeostatTest.py
@@ -0,0 +1,32 @@
+'''
+Created on Mar 11, 2013
+
+@author: stefano
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        """
+        Set up a new HomeoUnit with a connection to itself
+        """
+        unit1 = HomeoUnit new.
+    unit1 selfUpdate.              "allows the unit to have some values in its slots"
+    connection := HomeoConnection new.
+    connection incomingUnit: unit1.</body>
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
