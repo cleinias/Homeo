bf746c531984d2968d3f929d8754507f183e5c53
Stefano <stefano.franchi@gmail.com>
2013-03-11 22:48:30 -0500

    Ported all tests for HomeoUniselectorUniformRandom from Smalltalk code

---
+++ src/Core/HomeoUniselectorUniformRandom.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 11, 2013
+
+@author: stefano
+'''
+
+class HomeoUniselectorUniformRadom(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
