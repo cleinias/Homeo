53356700931ce29bcdffecaaa975348601baa45b
Stefano <stefano.franchi@gmail.com>
2013-03-18 12:42:31 -0500

    Added empty classes for HomeoUnitAristotelian and HomeoUnitNewtonian

---
+++ src/Core/HomeoUnitNewtonian.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 17, 2013
+
+@author: stefano
+'''
+
+class HomeoUnitNewtonian(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
