f2ca9c9c15ae5c5eeb4cc3a5b95efe4f129e126d
Stefano <stefano.franchi@gmail.com>
2013-03-19 12:05:59 -0500

    Ported all tests for HomeSimulationTest from Smalltalk code. Fixes #5

--- src/Simulator/HomeoSimulation.py
+++ src/Simulator/HomeoSimulation.py
@@ -1,1 +1,16 @@
+'''
+Created on Mar 19, 2013
 
+@author: stefano
+'''
+
+class HomeoSimulation(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
