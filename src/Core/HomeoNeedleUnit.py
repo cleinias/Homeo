116f2b99cd96f42209369ea0ed527f3d8b26b828
Stefano <stefano.franchi@gmail.com>
2013-03-19 18:28:50 -0500

    HomeoNeedleUnit and relative tests done and passed. Fixes #16

--- src/Core/HomeoNeedleUnit.py
+++ src/Core/HomeoNeedleUnit.py
@@ -3,15 +3,74 @@ Created on Feb 21, 2013
 
 @author: stefano
 '''
+import numpy
 
 class HomeoNeedleUnit:
     '''
-    classdocs
+    HomeoNeedleUnit holds parameters and methods describing the needle unit component of a homeostat unit
     '''
+    
+    ''' DefaultParameters is a  class variable holding the
+    default values of all the various parameters of future needle units.
+    All 'physical' values are expressed in internal units.  
+    Conversion to real physical units, if necessary, is done by Physical parameters dictionary in HomeoUnit
+    HomeoUnit initialize'''
 
+    DefaultParameters = dict(mass = 1.,                  # mass expressed in internal units
+                             surfaceArea = 1/numpy.pi,   # in internal units, eq. to a circle of radius 1 unit
+                             dragCoefficient = 1.        # dimensionless. used to compute drag acc to drag law for high velocities
+                             )
 
     def __init__(self):
         '''
-        Constructor
+        Read default parameters
         '''
+        
+        self._mass = HomeoNeedleUnit.DefaultParameters['mass']
+        self._surfaceArea = HomeoNeedleUnit.DefaultParameters['surfaceArea']
+        self._dragCoefficient = HomeoNeedleUnit.DefaultParameters['dragCoefficient']
+
+    @property
+    def mass(self):
+        return self._mass
+    
+    @mass.setter
+    def mass(self,aValue):
+        self._mass = aValue
+        
+    @property
+    def surfaceArea(self):
+        return self._surfaceArea
+    
+    @surfaceArea.setter
+    def surfaceArea(self, aValue):
+        self._surfaceArea = aValue
+        
+    @property    
+    def dragCoefficient(self):
+        return self._dragCoefficient
+    
+    @dragCoefficient.setter
+    def dragCoefficient(self,aValue):
+        self._dragCoefficient = aValue
+   
+        
+'''
+    class C:
+    def __init__(self):
+        self._x = None
+
+    @property
+    def x(self):
+        """I'm the 'x' property."""
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @x.deleter
+    def x(self):
+        del self._x
+'''
         
