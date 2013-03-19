82386ebeddda0f927f5bea300bcdf3016ea13d83
Stefano <stefano.franchi@gmail.com>
2013-03-19 08:08:06 -0500

    Ported all tests for HomeUniselectorAshby from ST code. Fixes #23

---
+++ src/Core/HomeoUniselectorAshby.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 18, 2013
+
+@author: stefano
+'''
+
+class HomeoUniselectorAshby(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
