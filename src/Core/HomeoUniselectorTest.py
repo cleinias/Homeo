129850eb459a5c3f82953522ce18ab274f9e3763
Stefano <stefano.franchi@gmail.com>
2013-03-20 11:16:44 -0500

    Completed unit tests for HomeoUniselector class. Fixes #29

--- src/Core/HomeoUniselectorTest.py
+++ src/Core/HomeoUniselectorTest.py
@@ -17,7 +17,7 @@ class HomeoUniselectorTest(unittest.TestCase):
 
 
     def setUp(self):
-        uniselector = HomeoUniselector()
+        self.uniselector = HomeoUniselector()
 
     def tearDown(self):
         pass
@@ -33,7 +33,7 @@ class HomeoUniselectorTest(unittest.TestCase):
             newRandomName = ''.join(random.choice(string.ascii_lowercase) for x in range(10))
             bogusClasses.extend(newRandomName)
         for currentClass in currentClasses:
-            self.assertTrue(HomeoUniselector.includeType(currentClass))
+            self.assertTrue(HomeoUniselector.includesType(currentClass))
         for bogusClass in bogusClasses:
             self.assertFalse(HomeoUniselector.includesType(bogusClass))
 
@@ -41,13 +41,33 @@ class HomeoUniselectorTest(unittest.TestCase):
         """
         Test that HomeoUniselector has a positive interval
         """
-        self.assertTrue((self.uniselector.upperBound() - self.uniselector.lowerBound()) >= 0)
+        self.assertTrue((self.uniselector.upperBound - self.uniselector.lowerBound) >= 0)
                         
     def testHomeoUniselectorTriggered(self):
         """
-        Test that HomeoUniselector is activated when its input exceeds the normal bounds
+        Cannot test that HomeoUniselector is activated when its input exceeds the normal bounds:
+        it is a HomeoUnit's responsibility to do so. HomeoUniselector only returns values when called 
         """
-        self.assertTrue(False)              #DOIT
+        self.assertTrue(True)              
+        
+    def testHomeoUniselectorIsAbstractClass(self):
+        '''
+        The core method of HomeUniselector is advance, which is only defined in its subclasses
+        '''
+        self.assertRaises(SubclassResponsibility, self.uniselector.advance)
+        
+    def testBasicAshbyRandom(self):
+        '''
+        Test the basic Ashbian mechanism of returning a random value equal to 1/25
+        of the interval between upperBound and lowerBound
+        '''
+        interval = self.uniselector.upperBound - self.uniselector.lowerBound
+        for i in xrange(1000):
+            value = self.uniselector.ashbyRandom()
+            self.assertAlmostEqual(abs(value),interval/25,delta = 0.0001)
+        
+        
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
