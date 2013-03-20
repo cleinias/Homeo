88d8470ee8034c6e5c4be18ddefec47b44b62eab
Stefano <stefano.franchi@gmail.com>
2013-03-20 11:19:38 -0500

    Completed HomeoUniselctor class with all tests passed. Fixes #15

--- src/Core/HomeoUniselector.py
+++ src/Core/HomeoUniselector.py
@@ -3,5 +3,94 @@ Created on Feb 19, 2013
 
 @author: stefano
 '''
+
+from Helpers.General_Helper_Functions  import *
+import numpy
+
 class HomeoUniselector(object):
-    pass
+    '''
+    Uniselector is the abstract class for different kinds of mechanisms producing random values 
+    for the connection between two units. UniselectorAshby replicate as much as possible 
+    the original stepping mechanism described by Ashby. UniselectorRandom provides a simpler mechanism 
+    that simply produces a random value in the admitted interval. 
+    Other mechanism can be added by subclassing Uniselector and 
+    providing an alternative method produceNewValue.
+    
+    Instance Variables:
+
+    lowerBound          the lower bound of the weight, default is 0
+    upperBound          the uperbound of the weight, default is 1
+    beeps               aBoolean that control whether the uniselector beeps when activated.
+    '''
+    
+    @classmethod
+    def includesType(self,aString):
+        '''
+        Answer true if aString represents the HomeoUniselector class or one of its subclasses
+        '''
+
+        uniselectorClasses = []
+        uniselectorClasses.extend([class_.__name__ for class_ in withAllSubclasses(HomeoUniselector)])
+        return aString in uniselectorClasses 
+
+    
+    def __init__(self):
+        "Initialize the HomeoUniselector instance"
+        
+        self.setDefaults()
+        
+    def setDefaults(self):
+        ''''sets the defaults for the uniselector weights. 
+        Can be (and usually is) overidden by subclasses'''
+
+        self._lowerBound = 0
+        self._upperBound = 1
+
+        self._beeps = False
+     
+    @property
+    def lowerBound(self):
+        return self._lowerBound
+    
+    @lowerBound.setter
+    def lowerBound(self, aValue):
+        self._lowerBound = aValue
+    
+    @property
+    def upperBound(self):
+        return self._upperBound
+    
+    @upperBound.setter
+    def upperBound(self, aValue):
+        self._upperBound = aValue
+    
+    @property
+    def beeps(self):
+        return self._beeps
+    
+    def toggleBeeping(self):
+        if self._beeps is None:
+            self._beeps = True
+        else:
+            self._beeps = not self._beeps
+    
+    def ashbyRandom(self):
+        '''
+        Produce 1 of a possible 25 different values for the weight 
+        by selecting at random a 25th of the interval defined by lowerBound and upperBound
+        
+        This is a very simplistic implementation of Ashby's algorithm that is improved
+        in the proper HomeoUniselector subclass 
+        '''
+        intervalSegment = (self._upperBound - self._lowerBound) / 25
+        randValue = numpy.random.uniform(1,25)
+        sign = round(numpy.random.uniform(-1,1))
+        
+        return randValue * intervalSegment * sign
+        
+    def advance(self):
+        '''control how to advance to the next position of the uniselector. 
+        Each subclass implements different mechanisms'''
+        
+        raise SubclassResponsibility()
+    
