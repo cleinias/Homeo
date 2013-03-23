cb23cfebd279087a8dac2ac9d707add4a6154b5c
Stefano <stefano.franchi@gmail.com>
2013-03-23 10:35:35 -0500

    minor edits in HomeoNeedleUnit

--- src/Core/HomeoNeedleUnit.py
+++ src/Core/HomeoNeedleUnit.py
@@ -54,23 +54,5 @@ class HomeoNeedleUnit:
     def dragCoefficient(self,aValue):
         self._dragCoefficient = aValue
    
-        
-'''
-    class C:
-    def __init__(self):
-        self._x = None
 
-    @property
-    def x(self):
-        """I'm the 'x' property."""
-        return self._x
-
-    @x.setter
-    def x(self, value):
-        self._x = value
-
-    @x.deleter
-    def x(self):
-        del self._x
-'''
         
