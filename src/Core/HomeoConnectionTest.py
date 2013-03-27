b511c14e344f0de6a2c80c927a9bcee2ffe7287b
Stefano <stefano.franchi@gmail.com>
2013-03-27 17:45:53 -0500

    HomeoConnection class completed. All but two tests pass. Two tests fails because they rely on missing functionalities in HomeoUnit

--- src/Core/HomeoConnectionTest.py
+++ src/Core/HomeoConnectionTest.py
@@ -6,6 +6,7 @@ Created on Mar 11, 2013
 from   HomeoUnit import *
 from   Homeostat import *
 import unittest, string, random
+from copy import copy
 
 class HomeoConnectionTest(unittest.TestCase):
 
@@ -15,9 +16,9 @@ class HomeoConnectionTest(unittest.TestCase):
         Set up a unit connected to itself
         """
         self.unit = HomeoUnit()
-        self.unit.selfUpdate()              #allows the unit to have some values in its slots"
+#        self.unit.selfUpdate()              #allows the unit to have some values in its slots"
         self.connection = HomeoConnection()
-        self.connection.incomingUnit(self.unit)
+        self.connection.incomingUnit = self.unit
 
     def tearDown(self):
         pass
@@ -25,15 +26,15 @@ class HomeoConnectionTest(unittest.TestCase):
 
     def testAddConnection(self):
         "Test the unit we setup is properly connected to itself"
-        self.assertTrue(self.connection.incomingUnit() == self.unit)
+        self.assertTrue(self.connection.incomingUnit == self.unit)
 
     def testDefaults(self):
         "Test default values for a connection "
-        self.assertTrue(self.connection.weight() >  0 and
-                        self.connection.weight() < 1)
-        self.assertTrue(self.connection.noise() > 0 and
-                        self.connection.noise() < 0.1)
-        self.assertTrue(self.connection.switch() is not None)
+        self.assertTrue(self.connection.weight >  0 and
+                        self.connection.weight < 1)
+        self.assertTrue(self.connection.noise > 0 and
+                        self.connection.noise < 0.1)
+        self.assertTrue(self.connection.switch is not None)
         
     def testSameAs(self):
         "Test if two connections are the same"
@@ -44,9 +45,9 @@ class HomeoConnectionTest(unittest.TestCase):
         unit2.setRandomValues()
 
         self.connection.randomizeConnectionValues()
-        self.connection.incomingUnit(self.unit)
+        self.connection.incomingUnit = self.unit
 
-        connection2 = self.connection.copy()
+        connection2 = copy(self.connection)
         self.assertTrue(self.connection.sameAs(connection2))
 
         self.connection.incomingUnit(unit2)
@@ -54,27 +55,27 @@ class HomeoConnectionTest(unittest.TestCase):
         
     def testState(self):
         "Test default state of the connection units" 
-        self.assertTrue(self.connection.state() == 'uniselector') 
+        self.assertTrue(self.connection.state == 'uniselector') 
 
         "switching from manual to uniselector and back"
-        self.connection.switchToUniselector()
-        self.assertTrue(self.connection.state() == 'uniselector')
+        self.connection.switchToUniselector
+        self.assertTrue(self.connection.state == 'uniselector')
 
         self.connection.switchToManual()
-        self.assertTrue(self.connection.state() == 'manual')
+        self.assertTrue(self.connection.state == 'manual')
             
     def testValueOfState(self):
         "Test connections are always in either 'uniselector' or 'manual' state"
 
-        self.assertTrue(self.connection.state() == 'manual' or self.connection.state() =='uniselector')
+        self.assertTrue(self.connection.state == 'manual' or self.connection.state =='uniselector')
             
-        self.connection.state('manual')
-        self.assertTrue(self.connection.state() == 'manual' or
-                        self.connection.state() == 'uniselector')
+        self.connection.state = 'manual'
+        self.assertTrue(self.connection.state == 'manual' or
+                        self.connection.state == 'uniselector')
 
-        self.connection.state('uniselector')
-        self.assertTrue(self.connection.state() == 'manual' or
-                        self.connection.state() == 'uniselector')
+        self.connection.state = 'uniselector'
+        self.assertTrue(self.connection.state == 'manual' or
+                        self.connection.state == 'uniselector')
 
         for testTry in xrange(10):
             randomString = ''.join(random.choice(string.ascii_lowercase) for x in range(10))
@@ -90,17 +91,17 @@ class HomeoConnectionTest(unittest.TestCase):
         outputWeighed = self.connection.output()
         inputUnit = self.connection.incomingUnit()
         
-        self.connection.noise(0)                  # eliminate noise"
+        self.connection.noise = 0                  # eliminate noise"
         self.assertTrue(outputWeighed - 
-                        ((inputUnit.currentOutput() * self.connection.weight() * self.connection.switch()) - self.connection.noise()) 
+                        ((inputUnit.currentOutput() * self.connection.weight * self.connection.switch) - self.connection.noise) 
                          < errorTolerance)
 
         "accounting for noise"
-        self.connection.noise(noiseLevel)                               #set noise"
+        self.connection.noise = noiseLevel                               #set noise"
     
         "the difference between the weighed connection output and the unit's value is at most equal to noise (plus the tolerance)"
         self.assertTrue(outputWeighed -
-                        abs(((inputUnit.currentOutput() * self.connection.weight() * self.connection.switch())) - self.connection.noise())
+                        abs(((inputUnit.currentOutput() * self.connection.weight * self.connection.switch)) - self.connection.noise)
                         < (noiseLevel + errorTolerance))
                 
 if __name__ == "__main__":
