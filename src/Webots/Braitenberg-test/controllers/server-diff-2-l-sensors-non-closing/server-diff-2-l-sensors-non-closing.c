167d2010f57854f59675fbad1f33a60a0c4a1096
Stefano Franchi <stefano.franchi@gmail.com>
2013-09-17 10:14:37 -0500

    Added a Webots world with one khepera robot implementing a tcp-ip server controller and two light sources. It will be used for phototaxis experiments (possibly the make up of the robot will have to be modified to narrow light sensors field of view). Added also a simple tcp-ip based python client implementing a Braitenberg type-2 light-seeking algorithm to be used as a test

--- src/Webots/Braitenberg-test/controllers/server-diff-2-l-sensors-non-closing/server-diff-2-l-sensors-non-closing.c
+++ src/Webots/Braitenberg-test/controllers/server-diff-2-l-sensors-non-closing/server-diff-2-l-sensors-non-closing.c
@@ -11,7 +11,8 @@
  */
 
 /* The protocol used in this example is taken from the Khepera serial       
- * communication protocol. It only supports 2 commands:                              
+ * communication protocol. It controls a Khepera-like robot
+ * with only 2 light sensors, and it supports only 2 commands:                              
  *                                                                           
  * D: set speed                                                              
  * O: read ambient light sensors                                             
@@ -30,24 +31,18 @@
 #include <string.h>
 #include <time.h>
 #include <sys/types.h>
-#ifdef WIN32
-#include <winsock.h>
-#else
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <netinet/in.h>         /* definition of struct sockaddr_in */
 #include <netdb.h>              /* definition of gethostbyname */
 #include <arpa/inet.h>          /* definition of inet_ntoa */
 #include <unistd.h>             /* definition of close */
-#endif
 
 #define SOCKET_PORT 10020
 #define NB_IR_SENSOR 2
 #define TIMESTEP 250
 
-static WbDeviceTag distance[NB_IR_SENSOR];
 static WbDeviceTag ambient[NB_IR_SENSOR];
-static WbDeviceTag led[2];
 static int fd;
 static fd_set rfds;
 
@@ -55,11 +50,7 @@ static int accept_client(int server_fd)
 {
     int cfd;
     struct sockaddr_in client;
-#ifndef WIN32
-    socklen_t asize;
-#else
     int asize;
-#endif 
     struct hostent *client_info;
 
     asize = sizeof(struct sockaddr_in);
@@ -81,16 +72,6 @@ static int create_socket_server(int port)
     int sfd, rc;
     struct sockaddr_in address;
 
-#ifdef WIN32
-    /* initialize the socket api */
-    WSADATA info;
-
-    rc = WSAStartup(MAKEWORD(1, 1), &info); /* Winsock 1.1 */
-    if (rc != 0) {
-        printf("cannot initialize Winsock\n");
-        return -1;
-    }
-#endif
     /* create the socket */
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sfd == -1) {
@@ -126,28 +107,12 @@ static void initialize()
     int i;
     char text[32];
 
-    /*  Disable distance sensors
-    for (i = 0; i < NB_IR_SENSOR; i++) {
-        sprintf(text, "ds%d", i);
-        distance[i] = wb_robot_get_device(text);
-        wb_distance_sensor_enable(distance[i], TIMESTEP);
-    }
-    */
     for (i = 0; i < NB_IR_SENSOR; i++) {
         sprintf(text, "ls%d", i);
         ambient[i] = wb_robot_get_device(text);
         wb_light_sensor_enable(ambient[i], TIMESTEP);
     }
 
-    /* disable led's
-    led[0] = wb_robot_get_device("led0");
-    led[1] = wb_robot_get_device("led1");
-    */
-
-    /* Enable the wheel encoders measurements */
-    /* Disabled
-    wb_differential_wheels_enable_encoders(TIMESTEP);
-    */
     printf("Khepera robot has been initialized by Webots\n");
     fd = create_socket_server(SOCKET_PORT);
     FD_ZERO(&rfds);
@@ -160,12 +125,8 @@ static void run()
     int ret;
     char buffer[256];
     int left_speed, right_speed;
-    short led_number, led_action;
-    static short led_value[2] = { 0, 0 };   /* initially off */
     struct timeval tv = { 0, 0 };
     int number;
-    int leftWheelEncoding;
-    int rightWheelEncoding;
 
     /* Set up the parameters used for the select statement */
 
@@ -189,38 +150,33 @@ static void run()
         printf("error reading from socket\n");
     }
     buffer[n] = '\0';
-    printf("Received %d bytes: %s\n", n, buffer);
+//    printf("Received %d bytes: %s\n", n, buffer);
 
     if (buffer[0] == 'D') {     /* set the speed of the motors */
         sscanf(buffer, "D,%d,%d", &left_speed, &right_speed);
         wb_differential_wheels_set_speed(left_speed, right_speed);
         send(fd, "d\r\n", 3, 0);
 
-    } else if (buffer[0] == 'O') {  /* read the ambient light sensor */
-        sprintf(buffer, "o,%d,%d,%d,%d,%d,%d,%d,%d\r\n",
+    } else if (buffer[0] == 'O') {  /* read the 2 ambient light sensor */
+        sprintf(buffer, "o,%d,%d\r\n",
                 (int)wb_light_sensor_get_value(ambient[0]),
-                (int)wb_light_sensor_get_value(ambient[1]),
-                (int)wb_light_sensor_get_value(ambient[2]),
-                (int)wb_light_sensor_get_value(ambient[3]),
-                (int)wb_light_sensor_get_value(ambient[4]),
-                (int)wb_light_sensor_get_value(ambient[5]),
-                (int)wb_light_sensor_get_value(ambient[6]),
-                (int)wb_light_sensor_get_value(ambient[7]));
+                (int)wb_light_sensor_get_value(ambient[1]));
         send(fd, buffer, strlen(buffer), 0);
 
     } else if (strncmp(buffer, "exit", 4) == 0) {
         printf("connection closed\n");
-#ifdef WIN32
-        closesocket(fd);
-        ret = WSACleanup();
-#else
         ret = close(fd);
-#endif
         if (ret != 0) {
             printf("Cannot close socket\n");
-        }
+            }
+	  fd = create_socket_server(SOCKET_PORT);
+	  FD_ZERO(&rfds);
+	  FD_SET(fd, &rfds);
+           return    
+  
         fd = 0;
-    } else {
+        } 
+        else {
         send(fd, "\n", 1, 0);
     }
 }
