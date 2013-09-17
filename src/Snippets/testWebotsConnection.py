8999eae4a7f9f0770c4983f1ee9a70d61c172976
Stefano Franchi <stefano.franchi@gmail.com>
2013-09-17 16:53:22 -0500

    Debugged Transducer TCP classes

--- src/Snippets/testWebotsConnection.py
+++ src/Snippets/testWebotsConnection.py
@@ -5,10 +5,31 @@ Created on Sep 17, 2013
 '''
 
 from RobotSimulator.WebotsTCPClient import WebotsTCPClient
+from RobotSimulator.Transducer import  *
 
 testClient = WebotsTCPClient()
 testClient._clientPort = 10020
 testSocket = testClient.getClientSocket()
+rightWheel = WebotsDiffMotorTCP('right')
+rightWheel.robotSocket = testSocket
+rightWheel.funcParameters = 10
+
+leftWheel = WebotsDiffMotorTCP('left')
+leftWheel.robotSocket = testSocket
+leftWheel.funcParameters = 10
+
+rightWheel.act()
+leftWheel.act()
+
+leftEye = WebotsLightSensorTCP(0)
+rightEye = WebotsLightSensorTCP(1)
+leftEye.robotSocket = testSocket
+rightEye.robotSocket = testSocket
+
+print "rightEye sees: ", rightEye.read()
+print "leftEye sees: ", leftEye.read()
+
+
 testClient.close()
 
 if __name__ == "__main__":
