b0b881f81318227c385d7114bc6fc35cbdad1c20
Stefano Franchi <stefano.franchi@gmail.com>
2013-10-09 19:02:52 -0500

    Set up a new webots world for the simulation of a homeo-controlled Braitenberg type 1-like  vehicle
    The khepera-like robot has one light sensor and one motor
    (the two motors always act in unison, with the same speed values)
    
    The world is flat, empty and with two sources of light

---
+++ src/Webots/Homeo-experiments/controllers/server-diff-1-l-sensors/build/server-diff-1-l-sensors.d
@@ -0,0 +1,8 @@
+build/release/server-diff-1-l-sensors.o: server-diff-1-l-sensors.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/differential_wheels.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/light_sensor.h \
+ /usr/local/webots/include/controller/c/webots/led.h
