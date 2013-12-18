13e7cbfeee1b2de8f273d4b1c5c2e90992e72604
Stefano Franchi <stefano.franchi@gmail.com>
2013-12-18 17:11:14 -0600

    Added a slightly modified version of the webots khepera tcp-ip server for Braitenberg type-2 experiments: 2 motors and 2 sensors

---
+++ src/Webots/Homeo-experiments/controllers/server-diff-2-light-sensors-2-motors/build/server-diff-2-light-sensors-2-motors.d
@@ -0,0 +1,9 @@
+build/release/server-diff-2-light-sensors-2-motors.o: \
+ server-diff-2-light-sensors-2-motors.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/differential_wheels.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/light_sensor.h \
+ /usr/local/webots/include/controller/c/webots/led.h
