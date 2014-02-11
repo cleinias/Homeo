982ae68a4a651f7dc5ce520363b6786d3d347a30
Stefano Franchi <stefano.franchi@gmail.com>
2014-02-11 14:59:08 -0600

    Added a second controller for orthodox Braitenberg type-2 vehicle implementing crossed connections between sensors and motors
     Changes to be committed:
    	new file:   src/Webots/Homeo-experiments/controllers/Type-2b-Orthodox-Braitenberg/Type-2b-Orthodox-Braitenberg.py

---
+++ src/Webots/Homeo-experiments/controllers/Type-2b-Orthodox-Braitenberg/Type-2b-Orthodox-Braitenberg.py
@@ -0,0 +1,55 @@
+#################################################################
+# First test controlling a khepera robot in a simple Webots world
+# simulating a straightforward Braitenberg-like type-2b vehicle:
+#
+# 2 sensors and 2 motors, with crossed connections
+#
+# Stefano Franchi 2014
+#################################################################
+from __future__ import division
+from controller import *
+import sys
+
+class Vehicle2 (DifferentialWheels):
+  counter = 1
+  def run(self):
+    self.leftEye = self.getLightSensor('ls0')
+    self.rightEye = self.getLightSensor('ls1')
+    self.leftEye.enable(32)
+    self.rightEye.enable(32)
+    counter = 1
+    maxSpeed = 100
+    maxLight = 1200
+    maxDelta = 0
+    while (self.step(32) != -1):
+      # Read the sensors:
+      # Take the complement of sensors' values, since webots' sensors return a value close to zero
+      # for maximum intensity and values close to maxLight for minimum intensity 
+      rightLightValue = maxLight - self.rightEye.getValue()
+      leftLightValue = maxLight - self.leftEye.getValue()
+      # Cross sensor data to opposite motor, as in a straightforward
+      # Braitenberg type-2b vehicle. 
+      # Also, scale sensor values to speed range :
+      rightSpeed = leftLightValue * (maxSpeed/maxLight)
+      leftSpeed = rightLightValue * (maxSpeed/maxLight)
+      
+      #=========================================================================
+      #
+      #     "Debugging info"
+      #
+      # speedDelta = abs(rightSpeed - leftSpeed)
+      # if speedDelta > maxDelta:
+      #     maxDelta = speedDelta
+      # print "Right eye: %d Left eye: %d Right Speed %f Left Speed %f Speed delta is %f Max so far is %f\r" % (rightLightValue,
+      #                                                                    leftLightValue,
+      #                                                                    rightSpeed,
+      #                                                                    leftSpeed,
+      #                                                                    speedDelta,
+      #                                                                    maxDelta)
+      # counter = counter + 1
+      #=========================================================================
+      self.setSpeed(leftSpeed, rightSpeed)
+
+robot = Vehicle2()
+robot.run()
+
