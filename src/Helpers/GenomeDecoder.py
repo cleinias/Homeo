759b499b72022de60b8065ab18effbd124b6088b
Stefano Franchi <stefano.franchi@gmail.com>
2015-01-02 12:38:49 -0600

    Refactored genome decoder utility to a function acting on a single genome list

--- src/Helpers/GenomeDecoder.py
+++ src/Helpers/GenomeDecoder.py
@@ -6,20 +6,22 @@ Utilities to interpret the genome of a Homeostat for further analysis
 from Core.HomeoUnit import *
 from Core.HomeoConnection import *
 import sys
+from twisted.protocols.ftp import FileNotFoundError
 
 def main(argv):
     try:
-        statFileGenomeDecoder(sys.argv[1], sys.argv[2])
+        statFileDecoder(sys.argv[1], sys.argv[2])
     except IndexError:
-        print "Usage: genomeDecoder fileIn fileOut [noUnits=]"
-    except:
+        print "Usage: statFileDecoder fileIn fileOut [noUnits=]"
+    except FileNotFoundError:
             print "You entered filename: ", sys.argv[1]
             print "File not found"
+    except:
+        raise            
             
-            
-def statFileGenomeDecoder(statFileIn,statFileOut, noUnits=6):
+def statFileDecoder(statFileIn,statFileOut, noUnits=6, rounding=2):
     """Read a GA stat file and convert the values in the genome
-       (all in (,1) range to the actual value used by the units.
+       (all in (,1) range) to the actual value used by the units.
        
        The GA stat file structure is composed by identical line containing
        Run Number
@@ -44,7 +46,7 @@ def statFileGenomeDecoder(statFileIn,statFileOut, noUnits=6):
         fileOut = open(statFileOut, "w")
     else:
         print "Usage: statFileGenomeDecoder fileIn fileOut"
-        print " Please enter a filename to write the decoded genome information to"
+        print "Please enter a filename to write the decoded genome information to"
         return
     lines = fileIn.readlines()
     fileIn.close()
@@ -60,24 +62,35 @@ def statFileGenomeDecoder(statFileIn,statFileOut, noUnits=6):
         fileOut.write(line[0]+"\t")              # Run number
         fileOut.write(line[1]+"\t")              # Final Distance
         fileOut.write(line[2]+"\t")              # Run Time
-        "Now reading units's values"
-        for unit in xrange(noUnits):
-                realMass = HomeoUnit.massFromWeight(float(line[(4*(unit+1))+0]))                      # Mass
-                fileOut.write(str(realMass)+"\t")
-                realViscosity = HomeoUnit.viscosityfromWeight(float(line[(4*(unit+1))+1]))            # Viscosity
-                fileOut.write(str(realViscosity)+"\t")
-                realTiming = HomeoUnit.uniselectorTimeIntervalFromWeight(float(line[(4*(unit+2))+2])) # UniselectorTiming (integer)                
-                fileOut.write(str(realTiming)+"\t")
-                realDeviation = HomeoUnit.maxDeviationFromWeight(float(line[(4*(unit+1))+3]))         # maxDeviation (integer)                        
-                fileOut.write(str(realDeviation)+"\t")
-        "now reading connection weights and writing them out unchanged"
-        for conn in xrange(noUnits*noUnits):
-            realConnWeight = HomeoConnection.connWeightFromGAWeight(float(line[(((4*noUnits)+3)+conn)]))
-            fileOut.write(str(realConnWeight)+"\t")
+        "Converting genome and printing to file"
+        lineAsFloats = [float(x) for x in line]
+        convertedGenome = genomeDecoder(noUnits, lineAsFloats[3:])
+        for value in convertedGenome:
+            fileOut.write(str(round(value, rounding))+"\t")
         fileOut.write("\n")        
     fileOut.close()
     
+def genomeDecoder(noUnits, genome):
+    """Convert the values in the genome to the actual value used by the units
+       Accepts a list of floats (values all in (0,1) range) whose length is: 
+       noUnits*essentVars + noUnits**2
+       Depends on Units having 4 essential variables
+       Return a list of actual values.
+    """
+    decodedValues = []
+    "Converting units' values"
+    for unit in xrange(noUnits):
+        decodedValues.append(HomeoUnit.massFromWeight(genome[(unit+1)+0]))                    # Mass
+        decodedValues.append(HomeoUnit.viscosityfromWeight(genome[(unit+1)+1]))               # Viscosity
+        decodedValues.append(HomeoUnit.uniselectorTimeIntervalFromWeight(genome[(unit+1)+2])) # UniselectorTiming (integer)                
+        decodedValues.append(HomeoUnit.maxDeviationFromWeight(genome[(unit+1)+3]))            # maxDeviation (integer)                        
+    
+    "Converting connection weights"
+    for conn in xrange(noUnits*noUnits):
+        decodedValues.append(HomeoConnection.connWeightFromGAWeight(genome[(4*noUnits)+conn]))
 
+    return decodedValues
+    
 if __name__ == "__main__":
     main(sys.argv[0])
     
