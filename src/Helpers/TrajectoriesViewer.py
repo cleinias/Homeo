77a6c7fed6c18982cfd707562a8a5cede2d59ce1
Stefano Franchi <stefano.franchi@gmail.com>
2015-01-11 02:06:09 -0600

    Improved GUI app for trajectory visualization and stats analysis.
    App now controls visualization of hall of fame results, as well
    as visualization of min, max, and average fitnesses.
    
    Changes to be committed:
    	modified:   src/Helpers/StatsAnalyzer.py
    	modified:   src/Helpers/TrajectoriesViewer.py

--- src/Helpers/TrajectoriesViewer.py
+++ src/Helpers/TrajectoriesViewer.py
@@ -3,18 +3,21 @@
 Created on Jan 2, 2015
 
 A small GUI utilities that allows the visualization of
-trajectory files   
+trajectory files and, if present, basic GA simulation data 
+from the related DEAP logbook
 @author: stefano
 '''
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import * 
 from Helpers.TrajectoryGrapher import graphTrajectory
+from Helpers.StatsAnalyzer import plotFitnessesFromLogBook, genomeAndFitnessList, indivsDecodedFromLogbook
 import sys
 from PyQt4.Qt import QMessageBox
 import os
 from glob import glob
 from sys import stderr
+import pickle
 
 
 class TrajectoryViewer(QWidget):
@@ -32,7 +35,10 @@ class TrajectoryViewer(QWidget):
         self._dirpath = dirPath
         self.buidGui()
         self.setDirpath(dirPath)
+        self._currentLogbookName = self.setCurrentLogbookName()
+        self.setCurrentLogbook()
         self.currentDir.setText(self._dirpath)
+        self.currentLogbookLE.setText(self._currentLogbookName)
         self.connectSlot()
         
         
@@ -40,29 +46,59 @@ class TrajectoryViewer(QWidget):
         '''
         Build the general GUI for the trajectory viewer
         '''       
-        self.setWindowTitle("Trajectory Viewer")
+        self.setWindowTitle("Trajectory and GA Statistics Viewer")
         self.setMinimumSize(700, 400)
 
         'layouts'
         self.overallLayout = QHBoxLayout()
         self.listLayout = QVBoxLayout()
         self.buttonsLayout = QVBoxLayout()
+        self.trajLayout = QHBoxLayout()
+        self.logLayout = QHBoxLayout()
+        self.currentDirLayout = QHBoxLayout()
+        self.trajectoriesLayout = QHBoxLayout()
+        
 
         'widgets'
         self.VisualizePB = QPushButton("Visualize")
         self.changeDirPB = QPushButton("Change Dir")             
         self.currentTrajEntry = QLineEdit(self)
+        self.currentLogbookLE = QLineEdit(self)
+        self.currentDirLabel = QLabel("Current dir: ")
         self.currentDir = QLineEdit(self)
+        self.trajListLabel = QLabel("Trajectories:")
         self.TrajList = QListWidget(self)
+        self.hallOfFamePB = QPushButton("Hall of Fame")
+        self.avgFitnessPB = QPushButton("Average Fitness")
+        self.genealogyPB = QPushButton("Genealogy")
+        self.trajLabel = QLabel("Trajectory:   ")
+        self.logLabel = QLabel("Logbook:     ")
         #self.listLayout.addWidget(self.QuitButton)
-        self.listLayout.addWidget(self.currentDir)
-        self.listLayout.addWidget(self.TrajList)
-        self.listLayout.addWidget(self.currentTrajEntry)
+        self.hofWidget = QTableWidget()
         
         'build layouts'
+        self.trajLayout.addWidget(self.trajLabel)
+        self.trajLayout.addWidget(self.currentTrajEntry)
+        self.logLayout.addWidget(self.logLabel)
+        self.logLayout.addWidget(self.currentLogbookLE)
+        self.currentDirLayout.addWidget(self.currentDirLabel)
+        self.currentDirLayout.addWidget(self.currentDir)
+        self.trajectoriesLayout.addWidget(self.trajListLabel)
+        self.trajectoriesLayout.addWidget(self.TrajList)
+
+
         self.buttonsLayout.addWidget(self.changeDirPB)
-        self.buttonsLayout.addStretch()
+        #self.buttonsLayout.addStretch()
+        self.buttonsLayout.addWidget(self.hallOfFamePB)
+        self.buttonsLayout.addWidget(self.avgFitnessPB)
+        self.buttonsLayout.addWidget(self.genealogyPB)
+        self.buttonsLayout.addStretch()        
         self.buttonsLayout.addWidget(self.VisualizePB)
+
+        self.listLayout.addLayout(self.currentDirLayout)
+        self.listLayout.addLayout(self.trajectoriesLayout)
+        self.listLayout.addLayout(self.logLayout)
+        self.listLayout.addLayout(self.trajLayout)
 
         self.overallLayout.addLayout(self.listLayout)
         self.overallLayout.addLayout(self.buttonsLayout)
@@ -73,6 +109,11 @@ class TrajectoryViewer(QWidget):
         self.TrajList.itemClicked.connect(self.onTrajectoryClicked)
         self.TrajList.itemDoubleClicked.connect(self.onTrajectoryDoubleClicked)
         self.changeDirPB.clicked.connect(self.getNewDir)
+        self.avgFitnessPB.clicked.connect(self.visualizeAvgFitnessGraph)
+        self.hallOfFamePB.clicked.connect(self.visualizeHOF)
+        self.genealogyPB.clicked.connect(self.visualizeGen)
+        self.hofWidget.cellDoubleClicked.connect(self.onCellDoubleClicked)
+        self.hofWidget.cellPressed.connect(self.onCellPressed)
         #self.QuitButton.clicked.connect(self.quit())
     
     def onTrajectoryDoubleClicked(self, curr):
@@ -81,6 +122,16 @@ class TrajectoryViewer(QWidget):
     
     def onTrajectoryClicked(self,curr):
         self.currentTrajEntry.setText(curr.text())
+        
+    def onCellPressed(self,row,column):
+        """Allow only selection of whole rows"""
+        self.hofWidget.selectRow(row)
+        
+    def onCellDoubleClicked(self,row,column):
+        """ Select the file corresponding to the double-clicked row and visualize 
+            the corresponding trajectory"""
+        "TO DO"
+        pass
     
     def visualize(self):
         if len(self.currentTrajEntry.text()) == 0:
@@ -94,15 +145,31 @@ class TrajectoryViewer(QWidget):
                 msgBox.setText("Invalid trajectory file");
                 msgBox.exec_();
     
+    def visualizeGen(self):
+        "visualize the individuals' genealogy"
+        self.notImplementedYet()
+        
+    def notImplementedYet(self):
+        msgBox = QMessageBox()
+        msgBox.setWindowTitle("NOT IMPLEMENTED!")
+        msgBox.setText("Sorry, this function is not implemented yet");
+        msgBox.exec_()
+ 
     def getNewDir(self):
         dir = QFileDialog.getExistingDirectory(parent = self, directory = self._dirpath)
         self.setDirpath(str(dir))
+        self.refreshLogbook()
+        self.currentLogbookLE.setText(self._currentLogbookName)        
         
     def quit(self):
-        app.quit()
+        pass
+        #app.quit()
             
     def supportedTrajExtensions(self):
-        return  ['txt', 'traj']
+        return  ['traj', 'txt']
+    
+    def supportedLogBookExtensions(self):
+        return ['lgb']
      
     def _trajectories(self):
         # Find the matching files for each valid
@@ -132,6 +199,77 @@ class TrajectoryViewer(QWidget):
         self._dirpath = dirpath
         self._populate()
         self.currentDir.setText(self._dirpath)
+        
+    def setCurrentLogbookName(self):
+        """Return the first logbook in current dir, i.e, the  
+           first filename with extension .lgb"""
+        logFilenames = []
+        for extension in self.supportedLogBookExtensions():
+            pattern = os.path.join(self._dirpath, '*.%s' % extension)
+            logFilenames.extend(os.path.basename(x) for x in glob(pattern))
+        try:
+            return logFilenames[0]
+        except IndexError:
+            return 'NO LOGBOOKS PRESENT'
+    
+    def setCurrentLogbook(self):
+        """Unpickle the logbook whose filename is in self._currentLogbookName
+           and store it in an iVar"""
+        if not self._currentLogbookName == "NO LOGBOOKS PRESENT":
+            try:
+                logbookFile = open(os.path.join(self._dirpath, self._currentLogbookName),'r')
+                self._currentLogbook=pickle.load(logbookFile)
+                logbookFile.close()                                                    
+            except IOError:
+                msgBox = QMessageBox()
+                msgBox.setText("No logbooks present");
+                msgBox.exec_()
+        else:
+            self._currentLogbook = None            
+    
+    def refreshLogbook(self):
+        self._currentLogbookName = self.setCurrentLogbookName()
+        self.setCurrentLogbook()
+                  
+    
+    def visualizeHOF(self):
+        """Print the hall of fame of 10 best individuals extracted 
+           from the logbook in a separate window".
+           Use a QTableWidget with a list containing
+        a list of headers at [0] and a list of rows at [1]"""
+        
+        self.hofWidget.clear()
+        self.hofWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        hof = genomeAndFitnessList(indivsDecodedFromLogbook(self._currentLogbook), num = 10)
+        self.hofWidget.setRowCount(len(hof[1]))
+        self.hofWidget.setColumnCount(len(hof[0]))
+
+        self.hofWidget = QTableWidget(len(hof[1]),len(hof[0]))
+        self.hofWidget.setHorizontalHeaderLabels(hof[0])
+        self.hofWidget.setWindowTitle("Hall of Fame: 10 best results")
+        for column in xrange(len(hof[0])):
+            for row in xrange(len(hof[1])):
+                try:
+                    item = QTableWidgetItem(str(round(hof[1][row][column],3)))
+                except TypeError:
+                    item = QTableWidgetItem(str(hof[1][row][column]))                    
+                item.setFlags(Qt.ItemIsSelectable |  Qt.ItemIsEnabled)
+                self.hofWidget.setItem(row,column,item)
+        self.hofWidget.setSortingEnabled(True)
+        self.hofWidget.sortByColumn(0,Qt.AscendingOrder)
+        self.hofWidget.show()
+    
+    def visualizeAvgFitnessGraph(self):
+        """Show a Matplotlib chart of fitnesses if data are present"""
+        
+        if self._currentLogbook is not None:
+            logbook = self._currentLogbook
+            plotFitnessesFromLogBook(logbook)
+        else:
+            msgBox = QMessageBox()
+            msgBox.setText("No logbook data to visualize");
+            msgBox.exec_();
+
     
 
 if __name__ == '__main__':
