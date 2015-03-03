4a1f654ba88f509296df5e25affdf152445ae87a
Stefano Franchi <stefano.franchi@gmail.com>
2015-03-03 12:01:44 -0600

    Added a Euclidean distance function and a ctype asByteArray function (both used by V-REP backend)

--- src/Helpers/General_Helper_Functions.py
+++ src/Helpers/General_Helper_Functions.py
@@ -1,4 +1,6 @@
 from __future__ import division
+from math import sqrt
+from ctypes import c_ubyte
 
 def withAllSubclasses(aClass):
     """
@@ -32,3 +34,9 @@ def scaleTo(fromRange,toRange, value):
        
     return (value - fromRange[0]) * (toRange[1]-toRange[0]) / (fromRange[1]-fromRange[0]) + toRange[0]
 
+def distance(pointA3D, pointB3D):
+    "Return Euclidean distance between two 3D points"    
+    return sqrt((pointA3D[0]-pointB3D[0])**2 + (pointA3D[1]-pointB3D[1])**2 + (pointA3D[2]-pointB3D[2])**2)
+
+def asByteArray(m_string):
+    return (c_ubyte * len(m_string)).from_buffer_copy(m_string)
